e4d16ef324fe8d6a8a23389634d6e228c753c530
Camila <camilasjc16#hotmail.com>
2021-07-07 19:33:24 -0300

    Initial Version Models

--- Autores/models.py
+++ Autores/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Cadastro(models.Model):
+    nome = models.CharField(max_length=50)
+    sobrenome = models.CharField(max_length=50)
+
+
+def __str__(self):
+    return self.nome
