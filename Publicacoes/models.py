0dd58341e202b4e0ef4c0099c54d7bda2e913834
Camila <camilasjc16#hotmail.com>
2021-07-07 20:28:31 -0300

    Commit Publicacoes A

--- Publicacoes/models.py
+++ Publicacoes/models.py
@@ -4,5 +4,5 @@ from django.conf import settings
 
 class Publicacao(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200, null=False)
-    description = models.TextField(max_length=200, null=False)
+    title = models.CharField(max_length=100, null=False)
+    description = models.TextField(max_length=300, null=False)
